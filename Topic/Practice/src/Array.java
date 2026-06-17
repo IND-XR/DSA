68c42ddda361d3336bff53791b130af7902b2a52
insta <anmolpreetsingh510@gmail.com>
2026-06-17 22:24:34 +0530

    Initial commit

---
+++ Topic/Practice/src/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+
+    public static void main(String[] args) {
+        // Array Declaration and Initialization
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print Array Elements
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
